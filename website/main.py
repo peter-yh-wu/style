0b77e3bd78e6454be7f7c5abd6daca7ba24c44d3
Eric Chiang <ericchiang101@gmail.com>
2020-01-14 10:54:06 -0800

    This includes rq for shakespere

--- website/main.py
+++ website/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, requests
 from rulebased import *
 app = Flask(__name__)
 
@@ -7,9 +7,14 @@ def about():
     return render_template("about.html")
 @app.route("/Made2Morph", methods = ['POST'])
 def morph():
-    first_name = request.form['transform_text']
+    input_text = request.form['transform_text']
+    post_process_text = contractions_func(input_text)
+    input_dict = {}
+    input_dict['input'] = post_process_text
+    r = requests.post('http://neu-style.appspot.com/submitted', data = input_dict)
+    return r.text
     #print(contractions_func(first_name))
-    return 'Hello %s %s have fun learning python <br/> <a href="/">Back Home</a>' % (contractions_func(first_name), 'hello')
+    #return 'Hello %s %s have fun learning python <br/> <a href="/">Back Home</a>' % (contractions_func(first_name), 'hello')
 if __name__ == "__main__":
     app.run()
 
