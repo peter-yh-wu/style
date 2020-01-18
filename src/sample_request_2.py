76a1724f23678f6388f19924e2f127a4d2906571
Peter Wu <peter.yh.wu@gmail.com>
2020-01-18 03:33:10 -0800

    Update sample_request_2.py

--- src/sample_request_2.py
+++ src/sample_request_2.py
@@ -8,11 +8,9 @@ import requests
 r = requests.post('http://34.82.174.216:5000/pos', data={'input': 'this is bad'})
 print(r.text)
 
-r = requests.post('http://34.82.174.216:5000/neg', data={'input': 'this is great'})
+# r = requests.post('http://34.82.174.216:5000/neg', data={'input': 'this is great'})
 
-r = requests.post('http://34.82.174.216:5000/trump', data={'input': 'turn this into trump sentence'})
-
-r = requests.post('http://34.82.174.216:5000/cardib', data={'input': 'turn this into cardi b sentence'})
+r = requests.post('http://34.83.30.177:5000/elon', data={'input': 'turn this into elon sentence'})
 
 print(r.text)
 
