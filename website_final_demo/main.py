0a485ba3448e9a08d3965782ebf1e239be3bc0cd
Lawrence Li <lawli@mit.edu>
2020-01-18 11:47:48 -0800

    Integrate front webpage

--- website_final_demo/main.py
+++ website_final_demo/main.py
@@ -13,8 +13,23 @@ def about():
 def formal():
     return render_template("translate.html")
 
+@app.route("/made2morph", methods = ['POST'])
+def morph():
+    input_text = request.form['transform_text']
+
+    """
+    post_process_text = contractions_func(input_text)
+    input_dict = {}
+    input_dict['input'] = post_process_text
+    r = requests.post('http://34.82.174.216:5000/Made2Morph', data=input_dict)
+    return (r.text)
+    """
+
+    #print(contractions_func(first_name))
+    return render_template("morph.html", input_text = input_text, contraction_input = contractions_func(input_text))
 
 @app.route("/formal/transfer", methods=['POST'])
+def transfer():
     input_text = request.form['text']
     r = requests.post('http://34.83.30.177:5000/elon', data={'input': input_text})
     return render_template('translateoutput.html', output = output)
