3a2ff559688696555e95befa2fbdceb11c5b2f1a
Lawrence Li <lawli@mit.edu>
2020-01-18 15:45:16 -0800

    Implement translate request using ajax

---
+++ website_final_demo/static/js/translate.js
@@ -0,0 +1,18 @@
+function translateText() {
+  let text = document.getElementById("text");
+
+  var formData = new FormData();
+  formData.append("input", "hi");
+
+  var request = new XMLHttpRequest();
+  request.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("outputtext").innerHTML = request.responseText;
+    }
+  };
+
+  //request.open("POST", "http://34.83.30.177:5000/elon")
+  request.open("POST", "http://localhost:5001/");
+  request.send(formData);
+  return false;
+}
