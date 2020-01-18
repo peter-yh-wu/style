7dfe2f1cc454adab87d478ec6730a23fb912a631
Peter Wu <peter.yh.wu@gmail.com>
2020-01-18 10:39:50 -0800

    Update sample_request_2.py

--- src/sample_request_2.py
+++ src/sample_request_2.py
@@ -5,14 +5,12 @@ import requests
 # r = requests.post('http://neu-style.appspot.com/submitted', data{})
 # r = requests.post('http://34.82.174.216:5000/Made2Morph', data={'input': 'why are you Romeo'})
 # r = requests.post('http://34.82.174.216:5000/Made2Morph', data={'input': 'why are you Romeo'})
-r = requests.post('http://34.82.174.216:5000/pos', data={'input': 'this is bad'})
-print(r.text)
 
 # r = requests.post('http://34.82.174.216:5000/neg', data={'input': 'this is great'})
 
 r = requests.post('http://34.83.30.177:5000/elon', data={'input': 'turn this into elon sentence'})
-
-r = requests.post('http://34.82.174.216:5000/obama', data={'input': 'turn this into obama sentence'})
-
 print(r.text)
 
+
+r = requests.post('http://34.82.174.216:5000/obama', data={'input': 'turn this into obama sentence'})
+print(r.text)
