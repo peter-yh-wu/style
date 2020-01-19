2c706a88f267378b2adfd06dc54ee91284074fee
Lawrence Li <lawli@mit.edu>
2020-01-18 18:06:20 -0800

    Add onclick to buttons

---
+++ website_final_demo/static/js/onclick.js
@@ -0,0 +1,9 @@
+function clickButton(name) {
+  var names = ["musk", "obama", "trump"];
+  for (var i = 0; i < names.length; i++) {
+    otherName = names[i];
+    document.getElementById(otherName).setAttribute("class", "unclicked");
+  }
+  document.getElementById(name).setAttribute("class", "clicked");
+}
+
