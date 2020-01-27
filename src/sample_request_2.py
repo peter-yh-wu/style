0e3dd296ae35fe00b79e20fa39946521d256cd7a
Peter Wu <peter.yh.wu@gmail.com>
2020-01-27 16:19:43 -0500

    Update sample_request_2.py

--- src/sample_request_2.py
+++ src/sample_request_2.py
@@ -1,20 +1,13 @@
 import requests
 
-# r = requests.get('http://neu-style.appspot.com/form')
 
-# r = requests.post('http://neu-style.appspot.com/submitted', data{})
-# r = requests.post('http://34.82.174.216:5000/Made2Morph', data={'input': 'why are you Romeo'})
-# r = requests.post('http://34.82.174.216:5000/Made2Morph', data={'input': 'why are you Romeo'})
-
-# r = requests.post('http://34.82.174.216:5000/neg', data={'input': 'this is great'})
-
-r = requests.post('http://34.83.30.177:5000/elon', data={'input': 'turn this into elon sentence'})
+r = requests.post('http://35.233.173.95:5000/elon', data={'input': 'turn this into elon sentence'})
 print(r.text)
 
 
-r = requests.post('http://34.82.174.216:5000/obama', data={'input': 'turn this into obama sentence'})
+r = requests.post('http://35.233.173.95:5000/obama', data={'input': 'turn this into obama sentence'})
 print(r.text)
 
 
-r = requests.post('http://104.196.253.238:5000/trump', data={'input': 'turn this into trump sentence'})
+r = requests.post('http://35.233.173.95:5000/trump', data={'input': 'turn this into trump sentence'})
 print(r.text)
